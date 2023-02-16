3dcb57bbb90bfadd166058714d67244334ff7e8f
ElmerLaptop <thellanajr@gmail.com>
2023-02-16 17:34:49 +0800

    added computer choice

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,1 +1,11 @@
+console.log('Hello World!');
 
+
+const getComputerChoice = () => {
+    const selection = ['Rock', 'Paper', 'Scissors'];
+    const randomIndex = Math.floor(Math.random()*selection.length);
+    return selection[randomIndex];
+}
+
+const randomIndex = getComputerChoice();
+console.log(randomIndex);
