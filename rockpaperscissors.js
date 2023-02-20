41e5fc93158fd2419827c2fdea4fc972e50414c9
ElmerLaptop <thellanajr@gmail.com>
2023-02-20 16:08:06 +0800

    added 1 round of rock, paper, scissors game

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -2,7 +2,7 @@ console.log('Hello World!');
 
 //store random computer decision
 const getComputerChoice = () => {
-    const selection = ['Rock', 'Paper', 'Scissors'];
+    const selection = ['rock', 'paper', 'scissors'];
     const randomIndex = Math.floor(Math.random()*selection.length);
     return selection[randomIndex];
 }
@@ -18,12 +18,30 @@ const getUserChoice = function() {
   };
 
 const myInput = getUserChoice();
-console.log(myInput);
+console.log(myInput); //hide when finished
   
   
-
-
-//play 1 round that uses user input against computer choice
-
-//result if user win/lose
-//5 round game that tracks the score at the end then announces it
+//play 1 round that uses user choice against computer choice
+const playRound = function(playerSelection, computerSelection) {
+    // Check for a tie
+    if (myInput === randomIndex) {
+      return "It's a tie!";
+    }
+  
+    // Check for player wins
+    if ((myInput === 'rock' && randomIndex === 'scissors') ||
+        (myInput === 'paper' && randomIndex === 'rock') ||
+        (myInput === 'scissors' && randomIndex === 'paper')) {
+      return "You win! " + myInput + " beats " + randomIndex;
+    }
+  
+    // If neither tie nor player wins, the computer wins
+    return "You lose! " + randomIndex + " beats " + myInput;
+  }
+  
+  // Test the function with player input and random computer input
+  const playerSelection = getUserChoice
+  const computerSelection = getComputerChoice
+  
+  console.log(playRound(playerSelection, computerSelection));
+//5 round game that tracks the score at the end then announces it (loops)
