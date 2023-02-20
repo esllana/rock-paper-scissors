1e02717788d25f2f5388154ce885c1a4a7ccf3bb
ElmerLaptop <thellanajr@gmail.com>
2023-02-20 11:19:25 +0800

    added user input

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,6 +1,6 @@
 console.log('Hello World!');
 
-
+//store random computer decision
 const getComputerChoice = () => {
     const selection = ['Rock', 'Paper', 'Scissors'];
     const randomIndex = Math.floor(Math.random()*selection.length);
@@ -8,4 +8,22 @@ const getComputerChoice = () => {
 }
 
 const randomIndex = getComputerChoice();
-console.log(randomIndex);
+console.log(randomIndex); //hide when finished
+
+//store user decision (case insensitive)
+const getUserChoice = function() {
+    let userInput = prompt("Let's play a game of Rock, Paper, Scissors! Type what you are going to play with:");
+    userInput = userInput.toLowerCase();
+    return userInput;
+  };
+
+const myInput = getUserChoice();
+console.log(myInput);
+  
+  
+
+
+//play 1 round that uses user input against computer choice
+
+//result if user win/lose
+//5 round game that tracks the score at the end then announces it
